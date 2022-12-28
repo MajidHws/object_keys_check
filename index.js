9b450b41cad2fdf0f5d861b97ca8c1ca53ede2b5
majidhws <majidhws@gmail.com>
2022-12-28 11:24:20 +0300

    refactoring

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const original = require('./data/original.json')
 const check = require('./data/check.json')
 
 const checkKeys = Object.keys(check)
-let notFound = []
 
-Object.keys(original).forEach(element => {
-    if(!checkKeys.includes(element)) {
-        console.log(element);
-        notFound.push(element)
-    }
-});
 
-console.log(`Total: ${notFound.length}`);
+console.log(`Total: ${Object.keys(original).filter(element =>!checkKeys.includes(element))
+    .map((element, i) => console.log(`${i+1}. ${element}`)).length}`);
 
 
