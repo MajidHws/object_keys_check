0cf3c607d86def14ded0b0b0371dab4e123f3999
majidhws <majidhws@gmail.com>
2022-12-28 11:25:02 +0300

    refactoring

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const check = require('./data/check.json')
 const checkKeys = Object.keys(check)
 
 
-console.log(`Total: ${Object.keys(original).filter(element =>!checkKeys.includes(element))
-    .map((element, i) => console.log(`${i+1}. ${element}`)).length}`);
+console.log(`Total: ${Object.keys(original).filter(element =>!checkKeys.includes(element)).map((element, i) => console.log(`${i+1}. ${element}`)).length}`);
 
 
